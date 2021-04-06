3e8e4767ae6c7a024ed4741b596d728c3d1e277c
lion <zqhusixia@163.com>
2021-04-06 15:28:42 +0800

    add picture loading lib

---
+++ app/src/main/java/com/wowly/common/lib/picture/PictureActivity.kt
@@ -0,0 +1,12 @@
+package com.wowly.common.lib.picture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wowly.common.lib.picture.R.*
+
+class PictureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
